359f004158bcf742efb47f12fda36c39b3318cbb
Weckest <lpeder@shaw.ca>
2023-10-17 12:53:40 -0700

    working log in page and custom user name

--- scripts/script.js
+++ scripts/script.js
@@ -6,4 +6,13 @@
 function sayHello() {
     //do something
 }
+
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
 //sayHello();    //invoke function
